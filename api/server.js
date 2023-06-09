cf2b760f86ebf8a5c775ab34e3a497a2ab3cbd63
euerika <euerika.soaresdossantos@gmail.com>
2023-06-09 17:47:37 -0300

    Alteração3

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 const app = jsonServer.create();
 
 const data = fs.readFileSync(path.resolve(__dirname, '../db.json'), 'utf8');
-// fs.writeFileSync('../db.json', data);
+//  fs.writeFileSync('../db.json', data);
 fs.writeFileSync('tmp/db.json', data);
 
 // const router = jsonServer.router('../db.json');
